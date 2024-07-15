fb31035051ca0e4b8038c8e0a32aeb15da88a2a9
Trishit Hazra <hazratrishit@gmail.com>
2024-07-15 11:32:59 +0530

    added copy to clipboard functionality  for color codes and added some finishing touches

--- script.js
+++ script.js
@@ -10,31 +10,42 @@ const rgb = document.querySelector('.rgb');
 // select the hex <p>
 const hex = document.querySelector('.hex');
 
-// function that generated a random color in rgb value and hex value
+// function that generates a random color in rgb value and hex value
 function generateRandomColor() {
-    // first generate a random value between 0 to 25 for rgb
-    // then convert the random value to  hexadecimal value  for hex
+    // first generate a random value between 0 to 255 for rgb
+    // then convert the random value to hexadecimal value for hex
 
     // red for rgb and hex
     const red = Math.floor(Math.random() * 256);
-    const redHex = red.toString(16);
+    const redHex = red.toString(16).padStart(2, '0');
 
     // green for rgb and hex
     const green = Math.floor(Math.random() * 256);
-    const greenHex = green.toString(16);
+    const greenHex = green.toString(16).padStart(2, '0');
 
     // blue for rgb and hex
     const blue = Math.floor(Math.random() * 256);
-    const blueHex = blue.toString(16);
+    const blueHex = blue.toString(16).padStart(2, '0');
 
     const rgbColor = `rgb(${red}, ${green}, ${blue})`;
-    const hexColor = `#${redHex}${greenHex}${blueHex}`
+    const hexColor = `#${redHex}${greenHex}${blueHex}`;
 
     // return both color codes as objects
-    return { rgbColor, hexColor }
+    return { rgbColor, hexColor };
 }
 
-// add click event listener on button and change the the background color of body
+// function to copy the color code to clipboard
+async function copyToClipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        alert('Color code copied!');
+    } catch (err) {
+        alert('Some error occurred while trying to copy the color code!');
+        console.error('Could not copy text:', err.message);
+    }
+}
+
+// add click event listener on button and change the background color of body
 button.addEventListener('click', () => {
     // call the generateRandomColor function and extract the values using object destructuring
     const { rgbColor, hexColor } = generateRandomColor();
@@ -45,4 +56,15 @@ button.addEventListener('click', () => {
     // change the textContent of the two paragraphs displaying color codes in rgb and hex
     rgb.textContent = rgbColor;
     hex.textContent = hexColor;
-})
+});
+
+// add event listeners to copy the color code
+rgb.addEventListener('click', (event) => {
+    const text = event.target.textContent;
+    copyToClipboard(text);
+});
+
+hex.addEventListener('click', (event) => {
+    const text = event.target.textContent;
+    copyToClipboard(text);
+});
